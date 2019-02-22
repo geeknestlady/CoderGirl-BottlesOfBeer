7616ba9d33dd2f0093df4e52497900e7c16afa2d
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-02-22 15:42:05 -0600

    Finished Homework

--- BottlesOfBeer/Program.cs
+++ BottlesOfBeer/Program.cs
@@ -6,6 +6,12 @@ namespace BottlesOfBeer
     {
         public static void Main()
         {
+            for (int i = 99; i > 0; i--)
+            {
+                int bottleNum = i;
+                int lastBottle = i - 1;
+                Console.WriteLine($"{bottleNum} bottles of beer on the wall. {bottleNum} bottles of beer. Take 1 down and pass it around, {lastBottle} bottles of beer on the wall." );
+            }
             // TODO: Output "99 bottles of beer on the wall.  99 bottles of beer.  Take 1 down and pass it around, 98 bottles of beer on the wall.";
             // Count down until there are no more bottles of beer on the wall.
 
